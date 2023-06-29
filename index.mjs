8310012d26f89c0a5f7fe77cc3a9f378f9fea8d4
minjeong073 <kim.mj80730@gmail.com>
2023-06-29 20:05:26 +0900

    update url (/show)

--- index.mjs
+++ index.mjs
@@ -3,12 +3,19 @@ import express from 'express'; // module 로 가져오는 방식
 
 const app = express();
 
+let visit  = 0;
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello' });
 });
 
 app.get('/home', (req, res) => {
+  visit += 1;
   res.json({ message: 'server OK!' });
+});
+
+app.get('/show', (req, res) => {
+  res.json({message: `${visit} 회`});
 });
 
 app.listen(5000, () => {
