53ca4ecdcfe1bcdb78348d1d8dcbdcdd1992148f
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-07-04 20:41:37 +0900

    [0706] db 연결

--- index.mjs
+++ index.mjs
@@ -1,7 +1,18 @@
 import express from 'express'; // module 로 가져오는 방식
 // const express = require('express'); // commonJS 로 가져오는 방식
+import pg from 'pg'; // DB 연결하기 위한 방식 1) connection, 2) connection pool
+// instance와 server 연결하는 방법
 
 const app = express();
+const pool = new pg.Pool({
+  host: 'database-1.curpzbnv2b39.ap-northeast-2.rds.amazonaws.com',
+  user: 'postgres',
+  password: 'seren*7631',
+  database: 'postgres',
+});
+
+// console.log(result);
+// console.log(result.rows);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello' });
@@ -9,6 +20,13 @@ app.get('/', (req, res) => {
 
 app.get('/home', (req, res) => {
   res.json({ message: 'server OK!' });
+});
+
+app.get('/student', async (req, res) => {
+  const client = await pool.connect();
+  const result = await client.query('SELECT * FROM student');
+  res.json(result.rows);
+  client.release();
 });
 
 app.listen(5000, () => {
